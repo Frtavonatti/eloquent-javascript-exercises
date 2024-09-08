14b5ba3cb07508b4d0d9289b9161551058142630
frtavonatti <frtavonatti@gmail.com>
2024-09-08 13:48:35 +0100

    Ejercicio 1: Haciendo un triángulo con bucles

---
+++ cap2.js
@@ -0,0 +1,9 @@
+// Ejercicio 1: Haciendo un triángulo con bucles
+let gato = '#'; 
+
+for (let index = 0; index < 3; index++) {
+    gato += '#';
+    console.log(gato);
+}
+
+// Ejercicio 2: FizzBuzz
